55e8fbbe4adf349fa6b1acc87f586ce551a023a6
Miyauchi Akira <miyauchoi@gmail.com>
2019-07-21 21:07:36 +0900

    Add the module file

---
+++ vue-mixin-namespace.js
@@ -0,0 +1,147 @@
+/*!
+ * vue-mixin-namespace.js v1.00
+ * (c) 2019 Miyauchi Akira
+ * Released under the MIT License.
+ */
+
+function namespaced_mixin({namespace, mixin}) {
+
+	const computed = {}
+
+	computed[namespace] = function() {
+
+		const result = {}
+
+		// globals (Symbolic Link)
+		if(mixin.globals) {
+			mixin.globals.forEach(name => {
+				Object.defineProperty(result, name, {
+					get: () => {
+						return this[name]
+					},
+
+					set: (value) => {
+						this[name] = value
+					},
+				})
+			})
+		}
+
+		// inject (Symbolic Link)
+		if(mixin.inject) {
+			mixin.inject.forEach(name => {
+				Object.defineProperty(result, name, {
+					get: () => {
+						return this[name]
+					},
+
+					set: (value) => {
+						this[name] = value
+					},
+				})
+			})
+		}
+
+		// data (Symbolic Link)
+		{
+			const names = Object.keys(mixin.data())
+
+			names.forEach(name => {
+				Object.defineProperty(result, name, {
+					get: () => {
+						return this[`namespaces__${namespace}`][name]
+					},
+
+					set: (value) => {
+						this[`namespaces__${namespace}`][name] = value
+					},
+				})
+			})
+		}
+
+		// computed (Symbolic Link)
+		if(mixin.computed) {
+			const names = Object.keys(mixin.computed)
+
+			names.forEach(name => {
+				Object.defineProperty(result, name, {
+					get: () => {
+						return this[`namespaces__${namespace}_${name}`]
+					},
+
+					set: (value) => {
+						this[`namespaces__${namespace}_${name}`] = value
+					},
+				})
+			})
+		}
+
+		// methods (Entity)
+		{
+			const methods = Object.fromEntries(Object.entries(mixin.methods).map(([name, func]) => ([name, func.bind(result)])))
+
+			Object.assign(result, methods)
+		}
+
+		return result
+	}
+
+	// data (Entity)
+	function data() {
+		const result = {}
+		result[`namespaces__${namespace}`] = mixin.data()
+		return result
+	}
+
+	// inject (Entity)
+	let inject = []
+	if(mixin.inject) {
+		inject = mixin.inject
+	}
+
+	// computed (Entity)
+	if(mixin.computed) {
+		Object.entries(mixin.computed).forEach(([key, value]) => {
+			computed[`namespaces__${namespace}_${key}`] = function() {
+				return value.bind(this[namespace])()
+			}
+		})
+	}
+
+	// watch (Entity)
+	const watch = {}
+	if(mixin.watch) {
+		Object.entries(mixin.watch).forEach(([key, value]) => {
+			
+			if(typeof value === 'function') {
+				watch[`${namespace}.${key}`] = function() {
+					value.bind(this[namespace])(...arguments)
+				}
+				
+			} else if(typeof value === 'object') {
+
+				const obj = {...value}
+				obj.handler = function() {
+					value.handler.bind(this[namespace])(...arguments)
+				}
+				watch[`${namespace}.${key}`] = obj
+			}
+		})
+	}
+
+	// created
+	let created = null
+	if(mixin.created) {
+		created = function() {
+			mixin.created.bind(this[namespace])()
+		}
+	}
+
+	return { data, inject, created, watch, computed }
+}
+
+export function with_namespace(mixins) {
+	return Object.entries(mixins).map(([namespace, mixin]) => {
+		return namespaced_mixin({namespace, mixin})
+	})
+}
